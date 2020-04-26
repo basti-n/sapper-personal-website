5a15bfcd647313fc1a13a9156d3785ca7543f7be
Sebastian Neumair <sebastianneumair@Sebastians-MBP.fritz.box>
2020-04-26 13:10:10 +0200

    Add Home Page Main Content

---
+++ src/components/texts/HeroText.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let text;
+  export let isActive = false;
+</script>
+
+<style lang="scss">
+  p {
+    font-family: "Bungee Hairline", cursive;
+    font-size: 2.75rem;
+
+    &.active {
+      font-family: "Bungee Inline", cursive;
+    }
+  }
+</style>
+
+<p class:active={isActive}>{text}</p>
