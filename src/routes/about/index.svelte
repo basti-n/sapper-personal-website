2bd3688f1b8ef7f45a2309c100ce7a1904e4581a
Sebastian <sebastian.neumair@gmail.com>
2021-07-12 22:42:42 +0200

    Update work description (de)

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
   export async function preload() {
-    const response = await this.fetch("about.json");
+    const response = await this.fetch('about.json');
     const cv = await response.json();
 
     return { cv };
@@ -8,24 +8,24 @@
 </script>
 
 <script>
-  import SummaryBar from "../../components/Summary-Bar.svelte";
-  import MilestoneSynopsis from "../../components/milestone/Milestone-Synopsis.svelte";
-  import MilestoneEvent from "../../components/milestone/Milestone-Event/Milestone-Event.svelte";
+  import SummaryBar from '../../components/Summary-Bar.svelte';
+  import MilestoneSynopsis from '../../components/milestone/Milestone-Synopsis.svelte';
+  import MilestoneEvent from '../../components/milestone/Milestone-Event/Milestone-Event.svelte';
 
   export let cv;
 
   const content = {
-    title: "PM 2 Dev",
+    title: 'PM 2 Dev',
     subtitle: `All About Building Great Products`,
     text: `Journey from the what to the how`,
     img: {
-      src: "images/pm-to-dev.svg",
-      alt: "product manager to developer illustration"
+      src: 'images/pm-to-dev.svg',
+      alt: 'product manager to developer illustration'
     }
   };
 
   const description = `
-  5 Jahre Erfahrung im digitalen Produkt- und IT-Projektmanagement.
+  Seit mehr als 6 Jahren begeistert an der täglichen Arbeit an und mit digitalen Produkten.
   Auf der Suche nach spannenden Digitalprojekten und motivierten Teams um gemeinsam Produkte zu konzipieren, entwickeln und an den Markt zu bringen.
   `;
 </script>
